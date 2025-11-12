3fa30a7bb784bd280cb5afbed56be2769800cca2
Simon <skuang64@gmail.com>
2025-11-11 22:23:44 -0500

    New layout, frontend, backend, both in one

--- backend/src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/db.ts
-import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-export const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
-  await mongoose.connect(MONGODB_URI);
-};
